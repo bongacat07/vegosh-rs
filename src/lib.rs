7532b51c80195aa0cbb63ace11f10be7893750d6
bongacat07 <vaibhavanandeh07@gmail.com>
2026-07-04 14:11:11 +0530

    Lib.rs modified

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,6 @@
-mod vegosh;
+pub mod vegosh;
 
-use vegosh::{Vegosh, clear, delete_key, get, init, insert, size};
-
-pub const TABLE_SIZE: usize = 1 << 21;
-pub const MASK: u32 = (TABLE_SIZE as u32) - 1;
-pub const MAX_KEYS: u32 = 1_000_000;
-
-pub const EMPTY: u8 = 0x00;
-pub const OCCUPIED: u8 = 0x01;
-
-pub const KEY_SIZE: usize = 16;
-pub const VALUE_SIZE: usize = 32;
+pub use crate::vegosh::Vegosh;
+pub use crate::vegosh::{
+    KEY_SIZE, MAX_KEYS, TABLE_SIZE, VALUE_SIZE, clear, delete_key, get, init, insert, size,
+};
